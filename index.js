b2ed3d1514e3c5255f1b366c26f1705314ac160d
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-23 21:17:31 +0530

    initial' '

--- index.js
+++ index.js
@@ -19,7 +19,61 @@ app.post('/api',(req,res)=>{
     console.log(intent);
     if(intent='Default Welome Intent'){
         res.status(200).json({
-            fulfillmentText : "Hi there this is default intent"
+            
+                "fulfillmentText": "This is a text response",
+                "fulfillmentMessages": [
+                  {
+                    "card": {
+                      "title": "card title",
+                      "subtitle": "card text",
+                      "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                      "buttons": [
+                        {
+                          "text": "button text",
+                          "postback": "https://assistant.google.com/"
+                        }
+                      ]
+                    }
+                  }
+                ],
+                "source": "example.com",
+                "payload": {
+                  "google": {
+                    "expectUserResponse": true,
+                    "richResponse": {
+                      "items": [
+                        {
+                          "simpleResponse": {
+                            "textToSpeech": "this is a simple response"
+                          }
+                        }
+                      ]
+                    }
+                  },
+                  "facebook": {
+                    "text": "Hello, Facebook!"
+                  },
+                  "slack": {
+                    "text": "This is a text response for Slack."
+                  }
+                },
+                "outputContexts": [
+                  {
+                    "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+                    "lifespanCount": 5,
+                    "parameters": {
+                      "param": "param value"
+                    }
+                  }
+                ],
+                "followupEventInput": {
+                  "name": "event name",
+                  "languageCode": "en-US",
+                  "parameters": {
+                    "param": "param value"
+                  }
+                }
+                
         });
     }
     else{
