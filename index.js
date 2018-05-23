01eda5a8587a5423045ff8494fbebbee98af491a
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-23 20:52:39 +0530

    inital

---
+++ index.js
@@ -0,0 +1,35 @@
+const express =require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const API_KEY1= "q86si59pft";
+const API_KEY2 = "ye1rpmx0tk";
+app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+  );
+  app.use(bodyParser.json());
+
+app.post('/api',(req,res)=>{
+    //var parameters= req.body.queryResult.parameters;
+    var parameters= req.body.queryResult.parameters;
+
+    
+    var intent=req.body.queryResult.intent.displayName;
+    console.log(intent);
+    if(intent='Default Welome Intent'){
+        res.status(200).json({
+            fulfillmentText : "Hi there this is default intent"
+        });
+    }
+    else{
+        res.status(201).json({
+            fulfillmentText:"This is not a default welcome intent"
+        })
+    }
+});
+
+
+
+
+app.listen(process.env.PORT || 80);
