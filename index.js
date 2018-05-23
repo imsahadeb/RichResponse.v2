30ecb3f875f573827809709abe10837f62980a5e
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-23 21:24:58 +0530

    hi

--- index.js
+++ index.js
@@ -35,50 +35,13 @@ app.post('/api',(req,res)=>{
                       ]
                     }
                   }
-                ],
-                "source": "example.com",
-                "payload": {
-                  "google": {
-                    "expectUserResponse": true,
-                    "richResponse": {
-                      "items": [
-                        {
-                          "simpleResponse": {
-                            "textToSpeech": "this is a simple response"
-                          }
-                        }
-                      ]
-                    }
-                  },
-                  "facebook": {
-                    "text": "Hello, Facebook!"
-                  },
-                  "slack": {
-                    "text": "This is a text response for Slack."
-                  }
-                },
-                "outputContexts": [
-                  {
-                    "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-                    "lifespanCount": 5,
-                    "parameters": {
-                      "param": "param value"
-                    }
-                  }
-                ],
-                "followupEventInput": {
-                  "name": "event name",
-                  "languageCode": "en-US",
-                  "parameters": {
-                    "param": "param value"
-                  }
-                }
-                
+                ]      
         });
     }
     else{
         res.status(201).json({
             fulfillmentText:"This is not a default welcome intent"
+            
         })
     }
 });
